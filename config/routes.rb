86fd4bdf8a7bb0fca7102408881a0af2269ced05
duskstone <jedgardromero@gmail.com>
2020-05-26 13:16:55 -0400

    fixed typo error, pluralization of pokemon

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
+
+  # https://pokedex/api/pokemon <= index / create url
+  # https://pokedex/api/pokemon/2 <= show
+
+  namespace :api, defaults: { format: :json } do 
+    resources :pokemon, only: [:create, :index, :show]
+  end 
 end
